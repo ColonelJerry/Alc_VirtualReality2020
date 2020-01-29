7646eca60278d61c34edd537e7635332000d30bb
Leo Montgomery <leo.m922@stu.nebo.edu>
2020-01-29 09:23:24 -0700

    controller

---
+++ Virtual_Reality_Project01/Assets/Scripts/TempController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempController : MonoBehaviour
+{
+    public float verticalInput;
+    public float horizontalInput;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        transform.position += Vector3.forward * verticalInput * Time.deltaTime * speed;
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.position += Vector3.right * horizontalInput * Time.deltaTime * speed;
+    }
+}
